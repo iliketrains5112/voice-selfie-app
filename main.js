dd6e2e78af565d90a62f456b5d89bad25b4b6c46
iliketrains <84407492+iliketrains5112@users.noreply.github.com>
2022-06-17 11:18:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start(){
+    console.log("this is start button")
+document.getElementById("textbox").innerHTML="";
+recognition.start();
+
+}
+recognition.onresult=function(event){
+    console.log(event)
+    var content=event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML=content;
+
+}
